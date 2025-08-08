9d0d0b220a2852a142416a3d56b224de06e49494
nnaphaaa phayakka <cherlynze@gmail.com>
2025-08-08 13:35:45 +0700

    Create  Intro to SQL.sql

---
+++ SQL /intro to SQL.sql
@@ -0,0 +1,10 @@
+--  intro to basic SQL
+
+  --1. create table 
+ CREATE TABLE employee (
+  id INT UNIQUE, 
+  name text, --1st column
+  department text, -- 2nd column
+  posiontion text, --3rd column
+  salary real --4th column
+);	
